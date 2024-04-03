84a4cc5202dd7a3403181d4409552a05b85e3bcc
Ney Thu Aung <neythuaung@gmail.com>
2024-04-03 13:45:13 +0630

    UI Issue fixed

--- src/components/layouts/Navbar.tsx
+++ src/components/layouts/Navbar.tsx
@@ -53,11 +53,7 @@ const Navbar = () => {
 
    return (
 
-<<<<<<< HEAD
-      <nav className="bg-white dark:bg-gray-900 fixed w-full top-0 start-0 border-b border-gray-200 dark:border-gray-600">
-=======
       <nav className="bg-gray-200 dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
->>>>>>> 5bbb308dc27ce6d0e5e2a7823a457a94ad45123f
          <div className="max-w-screen-full flex flex-wrap items-center justify-between mx-auto p-4">
             <Link href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
                {/* <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo"> */}
