785d30ae4d53b7c88f2a35af4bcb4516ebc83905
Thaw Zin Htoo <thawzinhtoo@comquas.com>
2024-04-18 23:22:14 +0700

    update

--- src/components/layouts/Sidebar.tsx
+++ src/components/layouts/Sidebar.tsx
@@ -129,11 +129,7 @@ const Sidebar = () => {
                                             width={32}
                                             height={32}
                                         /> */}
-                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" className="h-6 w-6">
-                                                <rect width="256" height="256" fill="none"></rect>
-                                                <line x1="208" y1="128" x2="128" y2="208" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="16"></line>
-                                                <line x1="192" y1="40" x2="40" y2="192" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="16"></line>
-                                            </svg>
+                                            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">EduGateways</span>
                                         </div>
                                         <nav className="flex flex-1 flex-col">
                                             <ul role="list" className="flex flex-1 flex-col gap-y-7">
